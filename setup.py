8491f2ed7b4d2aefd2111e5e8370a35541def1e5
Sean Kelly <kelly@seankelly.biz>
2014-06-27 09:03:02 -0500

    prep 1.0.4 rel

--- setup.py
+++ setup.py
@@ -11,8 +11,6 @@ import os.path
 _name        = 'eke.review'
 _version     = '1.0.4'
 _description = 'Review and Quality Control for the EDRN Knowledge Environment'
-_url         = 'http://cancer.jpl.nasa.gov/products/eke-review'
-_downloadURL = 'http://oodt.jpl.nasa.gov/dist/eke'
 _author      = 'Sean Kelly'
 _authorEmail = 'sean.kelly@jpl.nasa.gov'
 _license     = 'ALv2'
@@ -53,7 +51,7 @@ def _read(*rnames):
 
 _header = '*' * len(_name) + '\n' + _name + '\n' + '*' * len(_name)
 _longDescription = _header + '\n\n' + _read('README.rst') + '\n\n' + _read('docs', 'INSTALL.txt') + '\n\n' \
-    + _read('docs', 'HISTORY.txt') + '\n\n' + _read('docs', 'LICENSE.txt')
+    + _read('docs', 'HISTORY.txt') + '\n'
 open('doc.txt', 'w').write(_longDescription)
 
 setup(
@@ -61,7 +59,6 @@ setup(
     author_email=_authorEmail,
     classifiers=_classifiers,
     description=_description,
-    download_url=_downloadURL,
     entry_points=_entryPoints,
     extras_require=_extras,
     include_package_data=True,
@@ -72,7 +69,7 @@ setup(
     name=_name,
     namespace_packages=_namespaces,
     packages=find_packages(exclude=['ez_setup']),
-    url=_url,
+    url='https://github.com/EDRN/' + _name,
     version=_version,
     zip_safe=_zipSafe,
 )
